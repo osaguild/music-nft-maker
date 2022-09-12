7019ac6388d3b1ba5550bcb9da299cdf0f5558dd
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-12 09:45:21 +0900

    add IProtocol

---
+++ protocol/contracts/interface/IProtocol.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.9;
+
+interface IProtocol {
+    /**
+     * @dev stake MTE to protocol. approve amount of staking MTE to protocol first.
+     */
+    function stake(uint256 amount) external;
+
+    /**
+     * @dev withdraw MTE from protocol with APY.
+     */
+    function withdraw(uint256 amount) external;
+
+    /**
+     * @dev amount of MTE which account is staking.
+     */
+    function balanceOfStaking(address account) external view returns (uint256);
+
+    /**
+     * @dev amount of MTE which account earned by staking.
+     */
+    function balanceOfApy(address account) external view returns (uint256);
+
+    /**
+     * @dev check whether account can mint oringin token and fanfic token.
+     */
+    function canMint(address account) external view returns (bool);
+}
