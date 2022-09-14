5a047b2f5b3616a93ba4a3e64c2c27c7f32d0258
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-15 07:29:04 +0900

    add test of erc2981

---
+++ protocol/test/erc2981.test.ts
@@ -0,0 +1,101 @@
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+import { ERC2981MultipleRoyalties } from '../typechain-types'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { RoyaltyInfo, Address, calcRoyalty, printRoyalty } from './utils'
+
+describe('ERC2981MultipleRoyalties.sol', () => {
+  const defaultRoyalty: RoyaltyInfo = {
+    receiver: '', // set alice in before()
+    fraction: 1000, // 10%
+  }
+  const bobRoyalty: RoyaltyInfo = {
+    receiver: '', // set bob in before()
+    fraction: 2000, // 20%
+  }
+  const carolRoyalty: RoyaltyInfo = {
+    receiver: '', // set carol in before()
+    fraction: 3000, // 30%
+  }
+  const addresses: Address[] = []
+  let owner: SignerWithAddress
+  let alice: SignerWithAddress
+  let bob: SignerWithAddress
+  let carol: SignerWithAddress
+  let erc2981: ERC2981MultipleRoyalties
+
+  before(async () => {
+    // signers
+    const _signers = await ethers.getSigners()
+    owner = _signers[0] as SignerWithAddress
+    alice = _signers[1] as SignerWithAddress
+    bob = _signers[2] as SignerWithAddress
+    carol = _signers[3] as SignerWithAddress
+    // set constants
+    defaultRoyalty.receiver = alice.address
+    bobRoyalty.receiver = bob.address
+    carolRoyalty.receiver = carol.address
+    addresses.push({ name: 'alice', address: alice.address })
+    addresses.push({ name: 'bob', address: bob.address })
+    addresses.push({ name: 'carol', address: carol.address })
+    // deploy
+    const _factory = await ethers.getContractFactory('ERC2981MultipleRoyalties')
+    erc2981 = await _factory.deploy(owner.address, defaultRoyalty.receiver, defaultRoyalty.fraction)
+  })
+  it('check default royalty of 1000 price, tokenId is 1', async () => {
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(1, 1000)
+    expect(_receivers[0]).to.be.equals(defaultRoyalty.receiver)
+    expect(_amounts[0]).to.be.equals(calcRoyalty(defaultRoyalty, 1000))
+    printRoyalty(addresses, _receivers, _amounts, 1, 1000)
+  })
+  it('add royalty info of bob, tokenId is 1', async () => {
+    await erc2981.addRoyaltyInfo(1, bob.address, bobRoyalty.fraction)
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(1, 1000)
+    expect(_receivers[1]).to.be.equals(bob.address)
+    expect(_amounts[1]).to.be.equals(calcRoyalty(bobRoyalty, 1000))
+    printRoyalty(addresses, _receivers, _amounts, 1, 1000)
+  })
+  it('add royalty info of carol, tokenId is 1', async () => {
+    await erc2981.addRoyaltyInfo(1, carol.address, carolRoyalty.fraction)
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(1, 1000)
+    expect(_receivers[2]).to.be.equals(carol.address)
+    expect(_amounts[2]).to.be.equals(calcRoyalty(carolRoyalty, 1000))
+    printRoyalty(addresses, _receivers, _amounts, 1, 1000)
+  })
+  it('add royalty info of carol, tokenId is 2', async () => {
+    await erc2981.addRoyaltyInfo(2, carol.address, carolRoyalty.fraction)
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(2, 1000)
+    expect(_receivers[1]).to.be.equals(carol.address)
+    expect(_amounts[1]).to.be.equals(calcRoyalty(carolRoyalty, 1000))
+    printRoyalty(addresses, _receivers, _amounts, 2, 1000)
+  })
+  it('delete royalty of tokenId 2', async () => {
+    await erc2981.resetTokenRoyalty(2)
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(2, 1000)
+    expect(_receivers[0]).to.be.equals(defaultRoyalty.receiver)
+    expect(_amounts[0]).to.be.equals(calcRoyalty(defaultRoyalty, 1000))
+    expect(_receivers[1]).to.be.equals(undefined)
+    expect(_amounts[1]).to.be.equals(undefined)
+    printRoyalty(addresses, _receivers, _amounts, 2, 1000)
+  })
+  it('delete default royalty', async () => {
+    await erc2981.deleteDefaultRoyalty()
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(1, 1000)
+    expect(_receivers[0]).to.be.equals(bobRoyalty.receiver)
+    expect(_amounts[0]).to.be.equals(calcRoyalty(bobRoyalty, 1000))
+    expect(_receivers[1]).to.be.equals(carolRoyalty.receiver)
+    expect(_amounts[1]).to.be.equals(calcRoyalty(carolRoyalty, 1000))
+    printRoyalty(addresses, _receivers, _amounts, 1, 1000)
+    const [receivers_, amounts_] = await erc2981.royaltyInfo(2, 1000)
+    expect(receivers_[0]).to.be.equals(undefined)
+    expect(amounts_[0]).to.be.equals(undefined)
+    printRoyalty(addresses, receivers_, amounts_, 2, 1000)
+  })
+  it('delete royalty of tokenId 1', async () => {
+    await erc2981.resetTokenRoyalty(1)
+    const [_receivers, _amounts] = await erc2981.royaltyInfo(1, 1000)
+    expect(_receivers[0]).to.be.equals(undefined)
+    expect(_amounts[0]).to.be.equals(undefined)
+    printRoyalty(addresses, _receivers, _amounts, 1, 1000)
+  })
+})
--- protocol/test/utils.ts
+++ protocol/test/utils.ts
@@ -1,9 +1,21 @@
+import { BigNumber } from 'ethers'
+
 type Balance = {
   alice: number
   bob: number
   protocol: number
   stakingOfAlice: number
   stakingOfBob: number
+}
+
+type RoyaltyInfo = {
+  receiver: string
+  fraction: number
+}
+
+type Address = {
+  name: string
+  address: string
 }
 
 const calcReward = (numerator: number, denominator = 10000, amount: number, blockDiff: number) =>
@@ -24,4 +36,34 @@ const printBalance = (balance: Balance) => {
   )
 }
 
-export { calcReward, printBalance, Balance }
+const calcRoyalty = (info: RoyaltyInfo, salePrice: number) => {
+  const _royalty = (salePrice * info.fraction) / 10000
+  return BigNumber.from(_royalty)
+}
+
+const printRoyalty = (
+  addresses: Address[],
+  receivers: string[],
+  amounts: BigNumber[],
+  tokenId: number,
+  price: number
+) => {
+  let message = `tokenId: ${tokenId} price: ${price} `
+  for (let i = 0; i < addresses.length; i++) {
+    if (receivers.length == 0) {
+      message += `${addresses[i]?.name}: 0 `
+    } else {
+      for (let j = 0; j < receivers.length; j++) {
+        if (addresses[i]?.address === receivers[j]) {
+          message += `${addresses[i]?.name}: ${amounts[j]?.toNumber()} `
+          break
+        } else if (j === receivers.length - 1) {
+          message += `${addresses[i]?.name}: 0 `
+        }
+      }
+    }
+  }
+  console.log(message)
+}
+
+export { calcReward, printBalance, calcRoyalty, printRoyalty, Balance, RoyaltyInfo, Address }
