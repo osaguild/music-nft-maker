6fd8139fc36e46ede1ea99490f94bf8b992ecf7d
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-17 15:34:43 +0900

    use address config

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,18 +1,9 @@
 import { ethers } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { FanficToken__factory, Market__factory, OriginToken__factory, Protocol__factory } from '../typechain-types'
+import { address } from './config'
 
 async function main() {
-  // accounts
-  const defaultReceiver = '0x0f16af1BdFeA64f8B8733603D346479f5B2c7DF5' // sub_4
-  // contracts
-  const protocolAddress = '0x49273516c29a92Ba7152b0BD934091c838c71F7C'
-  const stakingAddress = '0xA4F19B9955b797f412dcE952C36AE4E9959414a2'
-  const mteAddress = '0xB034fE8111Bf487422961C8083cA7cd3eACd8986'
-  const fanficAddress = '0x94798Ac04e9A0694D3D352Ee711B00b99dDbE0eF'
-  const originAddress = '0xb9e51c795b03C840ABB2344e16866BAba09dADDD'
-  const marketAddress = '0x28e2362AA5091Db22BB483dC4420A31718135216'
-
   // account check
   const deployer = (await ethers.getSigners())[0] as SignerWithAddress
   const balance = (await deployer.getBalance()).toString()
@@ -26,12 +17,12 @@ async function main() {
 
   // deploy StakingToken !! need protocol address !!
   const stakingFactory = await ethers.getContractFactory('StakingToken')
-  const staking = await stakingFactory.deploy(protocolAddress)
+  const staking = await stakingFactory.deploy(address.PROTOCOL_CONTRACT)
   console.log('Staking address:', staking.address)
 
   // deploy MteToken !! need protocol address !!
   const mteFactory = await ethers.getContractFactory('MteToken')
-  const mte = await mteFactory.deploy(protocolAddress)
+  const mte = await mteFactory.deploy(address.PROTOCOL_CONTRACT)
   console.log('MteToken address:', mte.address)
 
   // deploy FanficToken
@@ -50,26 +41,26 @@ async function main() {
   console.log('Market address:', market.address)
 
   // set up FanficToken
-  await FanficToken__factory.connect(fanficAddress, deployer).setOriginToken(originAddress)
-  console.log('FanficToken.setOriginToken:', originAddress)
-  await FanficToken__factory.connect(fanficAddress, deployer).setProtocol(protocolAddress)
-  console.log('FanficToken.setProtocol:', protocolAddress)
-  await FanficToken__factory.connect(fanficAddress, deployer).setDefaultRoyalty(defaultReceiver, 1000)
-  console.log('FanficToken.setDefaultRoyalty:', defaultReceiver, 1000)
+  await FanficToken__factory.connect(address.FANFIC_CONTRACT, deployer).setOriginToken(address.ORIGIN_CONTRACT)
+  console.log('FanficToken.setOriginToken:', address.ORIGIN_CONTRACT)
+  await FanficToken__factory.connect(address.FANFIC_CONTRACT, deployer).setProtocol(address.PROTOCOL_CONTRACT)
+  console.log('FanficToken.setProtocol:', address.PROTOCOL_CONTRACT)
+  await FanficToken__factory.connect(address.FANFIC_CONTRACT, deployer).setDefaultRoyalty(address.SUB_4_ACCOUNT, 1000)
+  console.log('FanficToken.setDefaultRoyalty:', address.SUB_4_ACCOUNT, 1000)
 
   // set up Market
-  await Market__factory.connect(marketAddress, deployer).setFanficToken(fanficAddress)
-  console.log('Market.setFanficToken:', fanficAddress)
+  await Market__factory.connect(address.MARKET_CONTRACT, deployer).setFanficToken(address.FANFIC_CONTRACT)
+  console.log('Market.setFanficToken:', address.FANFIC_CONTRACT)
 
   // set up OriginToken
-  await OriginToken__factory.connect(originAddress, deployer).setProtocol(protocolAddress)
-  console.log('OriginToken.setProtocol:', protocolAddress)
+  await OriginToken__factory.connect(address.ORIGIN_CONTRACT, deployer).setProtocol(address.PROTOCOL_CONTRACT)
+  console.log('OriginToken.setProtocol:', address.PROTOCOL_CONTRACT)
 
   // set up Protocol
-  await Protocol__factory.connect(protocolAddress, deployer).setMteToken(mteAddress)
-  console.log('Protocol.setMteToken:', mteAddress)
-  await Protocol__factory.connect(protocolAddress, deployer).setStakingToken(stakingAddress)
-  console.log('Protocol.setStakingToken:', stakingAddress)
+  await Protocol__factory.connect(address.PROTOCOL_CONTRACT, deployer).setMteToken(address.MTE_CONTRACT)
+  console.log('Protocol.setMteToken:', address.MTE_CONTRACT)
+  await Protocol__factory.connect(address.PROTOCOL_CONTRACT, deployer).setStakingToken(address.STAKING_CONTRACT)
+  console.log('Protocol.setStakingToken:', address.STAKING_CONTRACT)
 }
 
 main()
