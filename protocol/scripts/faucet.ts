1ea6b020e4c3803deb826581079a1236d0864db5
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-17 16:03:51 +0900

    add faucet script

---
+++ protocol/scripts/faucet.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'hardhat'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { MteToken__factory, Protocol__factory } from '../typechain-types'
+import { address } from './config'
+
+async function main() {
+  // account check
+  const deployer = (await ethers.getSigners())[0] as SignerWithAddress
+  const balance = (await deployer.getBalance()).toString()
+  console.log('run contract with the account:', deployer.address)
+  console.log('account balance:', balance)
+  // faucet
+  const target = address.SUB_1_ACCOUNT
+  const amount = ethers.utils.parseEther('1000')
+  await Protocol__factory.connect(address.PROTOCOL_CONTRACT, deployer).provideLiquidity(target, 0, amount)
+  console.log('send', amount.toString(), 'to', target)
+  console.log(
+    'balance of',
+    target,
+    'is',
+    (await MteToken__factory.connect(address.MTE_CONTRACT, deployer).balanceOf(target)).toString(),
+    'MTE'
+  )
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
