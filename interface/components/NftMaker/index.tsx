b13a395c8a4ae91e09cd69767d11fc25a907abff
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-18 14:01:54 +0900

    add mint event of origin token

--- interface/components/NftMaker/index.tsx
+++ interface/components/NftMaker/index.tsx
@@ -1,12 +1,61 @@
-import { FunctionComponent } from 'react'
-import { Box, Button } from '@chakra-ui/react'
+import { FunctionComponent, useState } from 'react'
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Input,
+  ModalFooter,
+  RadioGroup,
+  Stack,
+  Radio,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { useContract } from '../../hooks/Contract'
 
 const NftMaker: FunctionComponent = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [tokenType, setTokenType] = useState('ORIGIN')
+  const [tokenUri, setTokenUri] = useState<string>()
+  const { originToken } = useContract()
+
+  const make = async () => {
+    if (tokenUri) {
+      const tx = await originToken?.mint(tokenUri)
+      const receipt = await tx?.wait()
+      const transfer = receipt?.events?.find((v) => v.event === 'Transfer')
+      if (transfer === undefined) throw new Error('transfer event is not found')
+    }
+  }
+
   return (
     <Box textAlign="center">
-      <Button verticalAlign="bottom" onClick={() => alert('Make New is clicked!')}>
+      <Button verticalAlign="bottom" onClick={onOpen} mt={10}>
         Make New
       </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Form</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <RadioGroup onChange={setTokenType} value={tokenType} mb={5}>
+              <Stack direction="row">
+                <Radio value="ORIGIN">Origin Token</Radio>
+                <Radio value="FANFIC">Fanfic Token</Radio>
+              </Stack>
+            </RadioGroup>
+            <Input placeholder="token uri" value={tokenUri} onChange={(e) => setTokenUri(e.target.value)} />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={make}>Make</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   )
 }
--- interface/hooks/Contract/index.ts
+++ interface/hooks/Contract/index.ts
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { providers } from 'ethers'
-import { Protocol, Protocol__factory, MteToken, MteToken__factory } from '../../typechain-types'
+import {
+  Protocol,
+  Protocol__factory,
+  MteToken,
+  MteToken__factory,
+  OriginToken__factory,
+  OriginToken,
+  FanficToken__factory,
+  FanficToken,
+} from '../../typechain-types'
 import { address } from '../../config/address'
 
 const useContract = () => {
   const [protocol, setProtocol] = useState<Protocol>()
   const [mteToken, setMteToken] = useState<MteToken>()
+  const [originToken, setOriginToken] = useState<OriginToken>()
+  const [fanficToken, setFanficToken] = useState<FanficToken>()
   const { library } = useWeb3React<providers.Web3Provider>()
 
   useEffect(() => {
     if (library) {
       setProtocol(Protocol__factory.connect(address.PROTOCOL_CONTRACT, library.getSigner()))
       setMteToken(MteToken__factory.connect(address.MTE_CONTRACT, library.getSigner()))
+      setOriginToken(OriginToken__factory.connect(address.ORIGIN_CONTRACT, library.getSigner()))
+      setFanficToken(FanficToken__factory.connect(address.FANFIC_CONTRACT, library.getSigner()))
     }
   }, [library])
 
-  return { protocol, mteToken }
+  return { protocol, mteToken, originToken, fanficToken }
 }
 
 export { useContract }
