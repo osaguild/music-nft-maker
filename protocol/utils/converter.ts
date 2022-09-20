e29956c02e515d206794c724f343a78322d169b1
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-20 13:00:07 +0900

    add converter

---
+++ protocol/utils/converter.ts
@@ -0,0 +1,30 @@
+import { address } from './config'
+
+const convertAddress = (_address: string) => {
+  switch (_address) {
+    case address.PROTOCOL_CONTRACT:
+      return 'PROTOCOL'
+    case address.STAKING_CONTRACT:
+      return 'STAKING'
+    case address.MTE_CONTRACT:
+      return 'MTE'
+    case address.FANFIC_CONTRACT:
+      return 'FANFIC'
+    case address.ORIGIN_CONTRACT:
+      return 'ORIGIN'
+    case address.MARKET_CONTRACT:
+      return 'MARKET'
+    case address.SUB_1_ACCOUNT:
+      return 'SUB_1'
+    case address.SUB_2_ACCOUNT:
+      return 'SUB_2'
+    case address.SUB_3_ACCOUNT:
+      return 'SUB_3'
+    case address.SUB_4_ACCOUNT:
+      return 'SUB_4'
+    default:
+      return _address
+  }
+}
+
+export { convertAddress }
