43953464077dfacde99678aa4f7421c5f6fd630f
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-24 18:44:46 +0900

    fix faucet

--- protocol/scripts/faucet.ts
+++ protocol/scripts/faucet.ts
@@ -1,26 +1,31 @@
 import { ethers } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { MteToken__factory, Protocol__factory } from '../typechain-types'
-import { address } from '../utils'
+import { MteToken__factory, MteToken, Protocol, Protocol__factory } from '../typechain-types'
+import { address, convertAddress } from '../utils'
+import { BigNumber } from 'ethers'
 
-async function main() {
-  // account check
-  const deployer = (await ethers.getSigners())[0] as SignerWithAddress
-  const balance = (await deployer.getBalance()).toString()
-  console.log('run contract with the account:', deployer.address)
-  console.log('account balance:', balance)
-  // faucet
-  const target = address.SUB_1_ACCOUNT
-  const amount = ethers.utils.parseEther('100')
-  await Protocol__factory.connect(address.PROTOCOL_CONTRACT, deployer).provideLiquidity(target, 0, amount)
-  console.log('send', amount.toString(), 'to', target)
+const withdraw = async (protocol: Protocol, mte: MteToken, to: string, amount: BigNumber) => {
+  await protocol.provideLiquidity(to, 0, amount)
   console.log(
-    'balance of',
-    target,
-    'is',
-    (await MteToken__factory.connect(address.MTE_CONTRACT, deployer).balanceOf(target)).toString(),
+    convertAddress(to),
+    'received',
+    ethers.utils.formatEther(amount),
+    'MTE / totalBalance is',
+    ethers.utils.formatEther(await mte.balanceOf(to)),
     'MTE'
   )
+}
+
+async function main() {
+  // set up
+  const deployer = (await ethers.getSigners())[0] as SignerWithAddress
+  const protocol = await Protocol__factory.connect(address.PROTOCOL_CONTRACT, deployer)
+  const mte = await MteToken__factory.connect(address.MTE_CONTRACT, deployer)
+  // faucet
+  await withdraw(protocol, mte, address.SUB_1_ACCOUNT, ethers.utils.parseEther('100'))
+  await withdraw(protocol, mte, address.SUB_2_ACCOUNT, ethers.utils.parseEther('100'))
+  await withdraw(protocol, mte, address.SUB_3_ACCOUNT, ethers.utils.parseEther('100'))
+  await withdraw(protocol, mte, address.SUB_4_ACCOUNT, ethers.utils.parseEther('100'))
 }
 
 main()
